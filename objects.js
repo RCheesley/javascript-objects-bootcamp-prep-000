48fa88cbe0e4766c14fff34527975152960d1325
Ruth Cheesley <ruth@ruthcheesley.co.uk>
2019-04-27 21:36:43 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {name:"title"};
 
 function updatePlaylist(playlist,name,title){
-  playlist.name = 'title';
+  Object.assign({}, playlist, { name: ['name'], title: ['title']})
   return playlist;
 }
