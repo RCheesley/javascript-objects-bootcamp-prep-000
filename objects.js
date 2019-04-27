ddaf8f57bc1f1be4eb46acc399de12a140037e2e
Ruth Cheesley <ruth@ruthcheesley.co.uk>
2019-04-27 21:49:39 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,6 @@
 var playlist = {artistName:"songTitle"};
 
 function updatePlaylist(playlist,artistName,songTitle){
-playlist.artistName = 'artistName';
-playlist.songTitle = 'songTitle';
+playlist.artistName = 'songTitle'
 return playlist;
   }
